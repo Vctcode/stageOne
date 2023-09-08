7e8eafb51403efeb627cf59fb23d931d1bfedd4b
vctcode <victorode01@gmail.com>
2023-09-08 12:44:31 +0100

    chore: change time format to ms

--- index.js
+++ index.js
@@ -6,18 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Get the current UTC date and time
     const currentUTCDate = new Date(currentDate.toISOString());
 
-    const hours = currentUTCDate.getUTCHours();
-    const minutes = currentUTCDate.getUTCMinutes();
-    const seconds = currentUTCDate.getUTCSeconds();
-
-    const timeFormat = hours >= 12 ? "PM" : "AM";
-    const hours12 = hours % 12 || 12;
-    const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
-    const secondsFormatted = seconds < 10 ? `0${seconds}` : seconds;
-
-    const time = `${hours12}:${minutesFormatted}:${secondsFormatted} ${timeFormat}`;
-
-    // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    const time = new Date().getTime();
     const dayOfWeek = currentDate.getUTCDay();
 
     const daysOfWeek = [
@@ -37,5 +26,5 @@ document.addEventListener("DOMContentLoaded", () => {
   updateDateTime();
 
   // Update the date and time every second (1000 milliseconds)
-  setInterval(updateDateTime, 1000);
+  setInterval(updateDateTime, 100);
 });
